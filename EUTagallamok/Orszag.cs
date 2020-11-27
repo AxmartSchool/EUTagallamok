111803a8d3090d7319adc47f54ad51c77eb0a692
Axmart <notaxmart@gmail.com>
2020-11-27 19:45:19 +0100

    Orszag class and beolvasas method

---
+++ EUTagallamok/Orszag.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EUTagallamok
+{
+    class Orszag
+    {
+        public string Nev { get; set; }
+        public DateTime CsatlakozasDatuma { get; set; }
+
+
+
+
+        public static List<Orszag> Beolvasas()
+        {
+            var output = new List<Orszag>();
+
+            var sr = new StreamReader(@"..\..\EUcsatlakozas.txt",Encoding.UTF8);
+            string[] tempTomb;
+            while (!sr.EndOfStream)
+            {
+
+                tempTomb = sr.ReadLine().Split(';');
+                output.Add(new Orszag() { Nev = tempTomb[0], CsatlakozasDatuma = DateTime.Parse(tempTomb[1]) });
+
+            }
+
+            sr.Close();
+
+            return output;
+
+
+        }
+    }
+}
--- EUTagallamok/Program.cs
+++ EUTagallamok/Program.cs
@@ -8,8 +8,18 @@ namespace EUTagallamok
 {
     class Program
     {
+
+        static List<Orszag> Orszagok;
+
         static void Main(string[] args)
         {
+            Orszagok = Orszag.Beolvasas();
+
+
+
+
+            Console.ReadKey();
+
         }
     }
 }
